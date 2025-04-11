9c04e9287c69f3a4a6f417d95564d902879d0728
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2025-04-11 10:16:49 +0200

    Top Verilog / Module VHDL

--- src/project.v
+++ src/project.v
@@ -16,4 +16,19 @@ module tt_um_draft (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  // All output pins must be assigned. If not used, assign to 0.
+//  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+
+  Alu #(G_N_BIT = 7) m_alu (
+    .i_s1(ui_in),
+    .i_s2(uio_in),
+    .o_res(uo_out)
+  )
+
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0};
+
 endmodule
